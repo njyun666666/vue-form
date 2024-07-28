2146512c7490acc98c73f6a56ce0b7382b92c3af
Jyun <njyun666666@gmail.com>
2024-07-28 15:32:40 +0800

    set language

--- src/stores/locale.ts
+++ src/stores/locale.ts
@@ -1,18 +1,17 @@
 import type { LangType } from '@/i18n/config'
 import '@/i18n/dayjs/zh-tw'
+import primeEN from '@/i18n/primelocale/en.json'
+import primeZH_TW from '@/i18n/primelocale/zh-tw.json'
 import { useStorage } from '@vueuse/core'
 import dayjs from 'dayjs'
 import { defineStore } from 'pinia'
+import { usePrimeVue } from 'primevue/config'
 import { useI18n } from 'vue-i18n'
-
-// import { usePrimeVue } from 'primevue/config'
-// import primeEN from '@/i18n/primelocale/en.json'
-// import primeZH_TW from '@/i18n/primelocale/zh-tw.json'
 
 export const useLocaleStore = defineStore('locale', () => {
   const lang = useStorage<LangType>('lang', 'zh-tw')
   const { locale } = useI18n()
-  // const primevue = usePrimeVue()
+  const primevue = usePrimeVue()
 
   const setLang = (val: LangType) => {
     lang.value = val
@@ -22,11 +21,11 @@ export const useLocaleStore = defineStore('locale', () => {
 
     switch (val) {
       case 'en':
-        // primevue.config.locale = primeEN.en
+        primevue.config.locale = primeEN.en
         break
 
       default:
-        // primevue.config.locale = primeZH_TW['zh-TW']
+        primevue.config.locale = primeZH_TW['zh-TW']
         break
     }
   }
