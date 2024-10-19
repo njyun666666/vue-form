88838199f3ee314b2cce130f9b311d874ed27cee
Jyun <njyun666666@gmail.com>
2024-10-19 21:54:42 +0800

    update PriveVue

--- src/primeVueConfig.ts
+++ src/primeVueConfig.ts
@@ -1,8 +1,5 @@
-//@ts-ignore
-import Aura from '@/presets/aura'
 import type { PrimeVueConfiguration } from 'primevue/config'
 
 export const primeVueConfig: PrimeVueConfiguration = {
-  unstyled: true,
-  pt: Aura
+  theme: 'none'
 }
