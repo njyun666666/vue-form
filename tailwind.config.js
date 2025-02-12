796a571f3929ed693e778d2f9eccd999de546bc5
Jyun <njyun666666@gmail.com>
2025-02-12 15:18:21 +0800

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import * as colors from 'tailwindcss/colors'
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: 'jit',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'selector',
   theme: {
