2965dc16d5de986607349b1f3639befc8e4a9f0e
Jyun <njyun666666@gmail.com>
2026-03-14 12:49:50 +0800

    feat: implement menu data structure and update menu handlers to use dynamic menu import

--- src/mocks/menuHandlers.ts
+++ src/mocks/menuHandlers.ts
@@ -1,49 +1,10 @@
 import appConfig from '@/appConfig'
-import type { MenuViewModel } from '@/libs/models/Menu/Menu'
+import { menus } from '@/faker/menu'
 import { menuService } from '@/libs/services/menuService'
 import { HttpResponse, http } from 'msw'
 
 export const menuHandlers = [
   http.get(`${appConfig.FORM_API}${menuService.menusUrl}`, () => {
-    return HttpResponse.json([
-      {
-        menuId: 'home',
-        menuName: 'Page.Home',
-        icon: 'fa-solid fa-house',
-        url: '/'
-      },
-      {
-        menuId: 'form',
-        menuName: 'Page.ApplicationForm',
-        icon: 'fa-solid fa-file-circle-plus',
-        url: '/form'
-      },
-      {
-        menuId: 'flow-list',
-        menuName: 'Page.FlowList',
-        icon: 'fa-solid fa-diagram-project',
-        url: '/flow/list'
-      },
-      {
-        menuId: 'org',
-        menuName: 'Org.Org',
-        icon: 'fa-solid fa-sitemap',
-        url: '',
-        children: [
-          {
-            menuId: 'org-dept',
-            menuName: 'Org.Dept',
-            icon: 'fa-solid fa-building',
-            url: '/org/dept'
-          },
-          {
-            menuId: 'org-user',
-            menuName: 'Org.User',
-            icon: 'fa-solid fa-user',
-            url: '/org/user'
-          }
-        ]
-      }
-    ] as MenuViewModel[])
+    return HttpResponse.json(menus)
   })
 ]
